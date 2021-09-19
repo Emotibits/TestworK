7066f9c9039a1f1eee4c3226c84e126664f94a18
Emotibits <113228730+Emotibits@users.noreply.github.com>
2021-09-19 18:58:29 +0000

    warning fixes

--- code/NewJFrame.java
+++ code/NewJFrame.java
@@ -1,4 +1,23 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- *
+ * and open the template in the editor.
+ */
+
+package securityz;
+
+import encrypt.Decryptor;
+import encrypt.Encryptor;
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException
