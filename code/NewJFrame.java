f6eafb316a9c668c5cc06f86e34a236c1d9a28fb
Emotibits <113228730+Emotibits@users.noreply.github.com>
2022-03-26 07:09:13 +0000

    Code reorganization.

--- code/NewJFrame.java
+++ code/NewJFrame.java
@@ -860,4 +860,11 @@ public class NewJFrame extends javax.swing.JFrame {
         javax.swing.GroupLayout ContactUsPLayout = new javax.swing.GroupLayout(ContactUsP);
         ContactUsP.setLayout(ContactUsPLayout);
         ContactUsPLayout.setHorizontalGroup(
-            ContactUsPLayout.createParallelGroup(javax.swing.GroupLayout.Align
+            ContactUsPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(ContactUsPLayout.createSequentialGroup()
+                .addGroup(ContactUsPLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(ContactUsPLayout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 743, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(ContactUsPLayout.createSequentialGroup()
+              
