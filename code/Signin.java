8cab0f1ae9176030b0b2552ea4adfa8d8b36df6d
Emotibits <113228730+Emotibits@users.noreply.github.com>
2023-01-09 22:50:09 +0000

    update from mattjhayes

--- code/Signin.java
+++ code/Signin.java
@@ -251,4 +251,16 @@ public class Signin extends javax.swing.JFrame {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jLabel8MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel8MouseClicked
+      this.dispose();
+      Login l =new Login();
+      l.setVisible(true);
+    }//GEN-LAST:event_jLabel8MouseClicked
