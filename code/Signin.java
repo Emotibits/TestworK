967d907499c6b79a020ba33b293096d7dff168dd
Emotibits <113228730+Emotibits@users.noreply.github.com>
2023-01-14 20:50:25 +0000

    minor ui change

--- code/Signin.java
+++ code/Signin.java
@@ -318,3 +318,19 @@ public class Signin extends javax.swing.JFrame {
                 }
         
             
+            
+         }
+            
+        }
+        
+    }//GEN-LAST:event_jLabel9MouseClicked
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+        
